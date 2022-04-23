9670c5874709b53152a997f750dfea408cd2e3e1
aws997 <aws.basim97@gmail.com>
2022-04-23 21:02:58 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(home: Otp());
+    return  GetMaterialApp(home: Login());
   }
 }
